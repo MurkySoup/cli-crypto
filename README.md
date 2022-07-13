bd02e80f3c485e2cc202136f0e43f61eccbe8278
Rick Pelletier <galiagante@gmail.com>
2022-07-13 09:43:19 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
 # cli-crypto
-Command-Line String Cryptography
+
+Command-Line String Cryptography: Intended for secure encryption and movement of data elements small enough to fit into command-line interfaces.
+
+## Prerequisites
+
+These utilties require Python 3.6+ (preferably 3.7+) and the following modules:
+
+* sys
+* os
+* argparse
+* argon2 (argon2-cffi)
+* base64
+* hashlib
+* Crypto (pycrypto)
+
+# How to Use
+
+These scripts are rather simple and self-explanatory:
+
+```
+usage: argon-keygen.py [-h] -s STRING
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -s STRING, --string STRING
+                        String to Hash
+```
+
+```
+usage: cli-crypto-aes-cfb.py [-h] [-k KEY] [-r ROUNDS] [-e ENCRYPT | -d DECRYPT]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -k KEY, --key KEY     key string
+  -r ROUNDS, --rounds ROUNDS
+                        number of hash cycles to apply to key string
+  -e ENCRYPT, --encrypt ENCRYPT
+                        string to encrypt
+  -d DECRYPT, --decrypt DECRYPT
+                        string to decrypt
+```
+
+There are two optional environment variables that can be set to help out:
+* CLI_CRYPTO_KEY (arbitrary length string value)
+* CLI_CRYPTO_ROUNDS (integer value)
+
+## Fair Warning
+
+Use at your own risk:
+* Like any security-related product, you are assuming an unknown level of potential risk and liability.
+* Neither warranty (expressed or implied) nor statement of suitability will be issued for these scripts.
+* Never use any security or cryptographic product without prior testing, knowledge of it's limitations and compliance with your preferred/required acceptance criteria.
+
+## Built With
+
+* [Python](https://www.python.org/) Designed by Guido van Rossum.
+
+## Author
+
+**Rick Pelletier**
+
+## License
+
+Copyright (C) 2022, Richard Pelletier
+
+```
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
---
+++ cli-crypto-aes-cfb.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+Command Line String Crypto - 0.8.3-BETA (do not distribute)
+By Rick Pelletier (galiagante@gmail.com), 06 April 2022
+Last Update: 17 June 2022
+
+Example given:
+
+# ./cli-crypto-aes-cfb.py -e "$(date +%FT%T.%3N%z) - West side bell tower at midnight. Bring hot dogs and jello."
+
+Output:
+
+I857BFgpJzkYlSTGRg0Tu3FxCRela/bxeiYrM19AJQIuqkkt7N4gV4X/8giBj5OSY68HVwZ177p6nMCIRUN+PrtLBBzH/5k/E764qahNXRsUu6VrN3hUCkXBP5902ZLxpkcUgbuwm1FODQ==
+
+Features:
+- Uses AES-256-CFB to perform en/decryption on strings via command-line interface.
+- Great for handling short messages over low-volume channels.
+- An encryption key string can be supplied via command-line argument or by setting an environment variable named 'CLI_CRYPTO_KEY'.
+- A hash cycling value can be supplied via command-line argument or by setting an environment variable named 'CLI_CRYPTO_ROUNDS'
+
+TODO:
+- Maybe a version of this using CTR mode is a better idea?
+"""
+
+
+import os
+import sys
+import base64
+import hashlib
+import argparse
+from Crypto import Random
+from Crypto.Cipher import AES
+
+
+def key_setup(key_string, rounds=1):
+  value = hashlib.sha256(key_string.encode('utf-8')).digest()
+
+  for k in range(0, rounds):
+    value = hashlib.sha256(value).digest()
+
+  return value
+
+
+def encrypt_string(key_string, message_string, rounds):
+  private_key = key_setup(key_string, rounds)
+
+  try:
+    rem = len(message_string) % 16
+    padded = str.encode(message_string) + (b'\0' * (16 - rem)) if rem > 0 else str.encode(message_string)
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(private_key, AES.MODE_CFB, iv, segment_size=128)
+    enc = cipher.encrypt(padded)[:len(message_string)]
+    return base64.b64encode(iv + enc).decode()
+  except:
+    return False
+
+
+def decrypt_string(key_string, message_string, rounds):
+  private_key = key_setup(key_string, rounds)
+
+  try:
+    message_string = base64.b64decode(message_string)
+    iv, value = message_string[:16], message_string[16:]
+    rem = len(value) % 16
+    padded = value + (b'\0' * (16 - rem)) if rem > 0 else value
+    cipher = AES.new(private_key, AES.MODE_CFB, iv, segment_size=128)
+    return (cipher.decrypt(padded)[:len(value)]).decode()
+  except:
+    return False
+
+
+
+if __name__ == '__main__':
+  exit_value = 0
+  rounds = 1
+
+  parser = argparse.ArgumentParser()
+
+  option_group = parser.add_argument('-k', '--key', help='key string', type=str)
+  option_group = parser.add_argument('-r', '--rounds', help='number of hash cycles to apply to key string', type=int)
+
+  command_group = parser.add_mutually_exclusive_group()
+  command_group.add_argument('-e', '--encrypt', help='string to encrypt', type=str)
+  command_group.add_argument('-d', '--decrypt', help='string to decrypt', type=str)
+
+  args = parser.parse_args()
+
+  if args.key:
+    key_string = args.key
+  elif os.environ['CLI_CRYPTO_KEY']:
+    key_string = os.environ['CLI_CRYPTO_KEY']
+  else:
+    key_string = None
+
+  if args.rounds:
+    rounds = args.rounds
+  elif os.environ['CLI_CRYPTO_ROUNDS']:
+    rounds = int(os.environ['CLI_CRYPTO_ROUNDS'])
+
+  if key_string:
+    if args.encrypt:
+      encrypted_string = encrypt_string(key_string, args.encrypt, rounds)
+
+      if encrypted_string:
+        print(encrypted_string)
+      else:
+        print('Encryption error')
+        exit_value = 1
+
+    elif args.decrypt:
+      decrypted_string = decrypt_string(key_string, args.decrypt, rounds)
+
+      if decrypted_string:
+        print(decrypted_string)
+      else:
+        print('Decryption error')
+        exit_value = 1
+
+    else:
+      print('Use --help to see command line options')
+      exit_value = 1
+  else:
+    print('No valid key string presented')
+    exit_value = 1
+
+  sys.exit(exit_value)
+else:
+  sys.exit(1)
+
+# end of script
